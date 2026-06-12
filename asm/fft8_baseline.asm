363e1f10601584b53bf02e0471759da0c4668a1b
YiMing Jiang <2024040904012@std.uestc.edu.cn>
2026-06-12 09:13:58 +0800

    Create asm baseline source file

---
+++ asm/fft8_baseline.asm
@@ -0,0 +1,9 @@
+; fft8_baseline.asm
+; MCU FFT baseline assembly source.
+; This file will be generated or filled from mem/instr_fft8.mem later.
+
+.equ TEST_BASE,   0x1000
+.equ VERIFY_BASE, 0x2000
+.equ C707,        0x5a82
+
+HALT
